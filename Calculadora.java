2f94612371d60dce47313c996627e7cb8d200200
unknown <Administrador@LABORATORIO.LOCAL>
2020-02-20 07:53:49 -0300

    Subtracao

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,11 @@
 Inicio
-Declare valor1 int, valor2 int, soma int;
+Declare valor1 int, valor2 int, soma int, sub int;
 Exiba 'Digite o primeiro valor:';
 Leia valor1;
 Exiba 'Digite o segundo valor:';
 Leia valor2;
 soma = valor1 + valor2;
+sub = valor1 - valor2;
 Exiba 'A soma entre eles é:' + soma;
+Exiba 'A subtração entre eles é:' + sub;
 Fim
