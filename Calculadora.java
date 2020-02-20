4f52672c8b3279fa1696faf372d81dabea0b7705
unknown <Administrador@LABORATORIO.LOCAL>
2020-02-20 07:49:37 -0300

    Soma

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+Inicio
+Declare valor1 int, valor2 int, soma int;
+Exiba 'Digite o primeiro valor:';
+Leia valor1;
+Exiba 'Digite o segundo valor:';
+Leia valor2;
+soma = valor1 + valor2;
+Exiba 'A soma entre eles é:' + soma;
+Fim
