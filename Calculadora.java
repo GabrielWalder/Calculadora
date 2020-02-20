1817a6dd48918b36a434260d689365759261d765
unknown <Administrador@LABORATORIO.LOCAL>
2020-02-20 08:00:15 -0300

    Calculadora concluida

--- Calculadora.java
+++ Calculadora.java
@@ -1,11 +1,15 @@
 Inicio
-Declare valor1 int, valor2 int, soma int, sub int;
+Declare valor1 int, valor2 int, soma int, sub int, multi int, div int;
 Exiba 'Digite o primeiro valor:';
 Leia valor1;
 Exiba 'Digite o segundo valor:';
 Leia valor2;
 soma = valor1 + valor2;
 sub = valor1 - valor2;
+multi = valor1 * valor2;
+div = valor1 / valor2;
 Exiba 'A soma entre eles é:' + soma;
 Exiba 'A subtração entre eles é:' + sub;
+Exiba 'A multiplicação entre eles é:' + multi;
+Exiba 'A divisão entre eles é:' + div;
 Fim
